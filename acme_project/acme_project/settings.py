d6e256968c20f598ace4a6721b0731ac1ddb1c96
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-07-28 23:43:50 +0300

    Спринт 7/25 → Тема 2/8: Работа с формами → Урок 5/10 ДОРАБОТАНО

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
+    'django_bootstrap5',
     'pages.apps.PagesConfig',
 ]
 
